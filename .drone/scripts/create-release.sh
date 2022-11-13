694bd6f8db5a62581c342cb4d66e3853ba970a59
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-11-13 00:11:49 -0600

    Don't build targets in CI
    It's not working and I don't have the mental energy to figure that out right now. We'll figure it out later if a user requests it.

--- .drone/scripts/create-release.sh
+++ .drone/scripts/create-release.sh
@@ -7,30 +7,12 @@ if echo "${DRONE_COMMIT_MESSAGE}" | grep -q 'GH SKIP'; then
 fi
 
 .drone/scripts/setup-pbmpr.sh
-sudo apt-get install gh parse-changelog rustup -y
-rustup default stable
-
-rustup_targets=(
-    'aarch64-unknown-linux-gnu'
-    'i686-pc-windows-gnu'
-    'i686-pc-windows-msvc'
-    'i686-unknown-linux-gnu'
-    'x86_64-apple-darwin'
-    'x86_64-pc-windows-gnu'
-    'x86_64-pc-windows-msvc'
-    'x86_64-unknown-linux-gnu'
-)
-gh_release_assets=()
-for target in "${rustup_targets[@]}"; do
-    rustup target add "${target}"
-    cargo build --target "${target}" --release
-    gh_release_assets+=("${PWD}/target/${target}/release/shoot#shoot-${target}")
-done
+sudo apt-get install gh parse-changelog -y
 
 source makedeb/PKGBUILD
 
 release_notes="$(parse-changelog CHANGELOG.md "${pkgver}")"
 echo "${github_api_key}" | gh auth login --with-token
-gh release create "v${pkgver}" --title "v${pkgver}" --target "${DRONE_COMMIT_SHA}" -n "${release_notes}" "${gh_release_assets[@]}"
+gh release create "v${pkgver}" --title "v${pkgver}" --target "${DRONE_COMMIT_SHA}" -n "${release_notes}"
 
 # vim: set sw=4 expandtab:
